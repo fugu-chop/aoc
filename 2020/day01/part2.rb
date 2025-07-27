947491063ae0eea25993641043f35350ff592275
Dean Wan <dean@prodincident.com>
2025-07-28 09:01:07 +1000

    feat: add day1 part 2

--- 2020/day01/part2.rb
+++ 2020/day01/part2.rb
@@ -1,10 +1,23 @@
 # Part 2
 
 TARGET = 2020
-results_hash = {}
+results = []
 
-# Slurp file contents into hash
+# Slurp file contents into array
 File.foreach("/Users/dean/Documents/aoc/2020/day01/input.txt") do |result|
-    results_hash[result.to_i] = result.to_i
+  results << result.to_i
 end
 
+results.each_with_index do |num1, idx1| 
+  results.slice(idx1+1, results.length-1).each_with_index do |num2, idx2|
+    if num1 + num2 >= TARGET
+      next
+    end
+
+    results.slice(idx2+1, results.length-2).each_with_index do |num3, idx3|
+      if num1 + num2 + num3 == TARGET 
+        puts num1*num2*num3
+      end
+    end
+  end
+end
